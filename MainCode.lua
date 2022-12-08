6e1434355d2dc71bf1c3345188e6f71811c3aba3
Nuuuub01 <120091560+Nuuuub01@users.noreply.github.com>
2022-12-08 17:49:18 +0800

    Update MainCode.lua

--- MainCode.lua
+++ MainCode.lua
@@ -1,29 +1,32 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
-local Window = Rayfield:CreateWindow({
-	Name = "Made For Roblox Decendance",
-	LoadingTitle = "Powered By DrxTeamSupport",
-	LoadingSubtitle = "Game Found!!",
-	ConfigurationSaving = {
-		Enabled = true,
-		FolderName = nil, -- Create a custom folder for your hub/game
-		FileName = "Big Hub"
-	},
-        Discord = {
-        	Enabled = false,
-        	Invite = "sirius", -- The Discord invite code, do not include discord.gg/
-        	RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-        },
-	KeySystem = false, -- Set this to true to use our key system
-	KeySettings = {
-		Title = "Sirius Hub",
-		Subtitle = "Key System",
-		Note = "Join the discord (discord.gg/sirius)",
-		FileName = "SiriusKey",
-		SaveKey = true,
-		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = "Hello"
-	}
-})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("TITLE", "DarkTheme")
 
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
-local Section = Tab:CreateSection("Section Example")
+local Tab1 = Window:NewTab("Main")
+local Tab1Section = Tab1:NewSection("Main / Features")
+
+local Tab2 = Window:NewTab("Animation")
+local Tab2Section = Tab2:NewSection("Change ur Avatar Animation")
+
+Tab1Section:NewLabel("Powered By DrxTeam/Support")
+
+Tab1Section:NewButton("Tp To Buttons", "Pretty Much The Title", function()
+    print("Clicked")
+end)
+
+Tab1Section:NewButton("FullBright", "Once Die Execute", function()
+    print("Clicked")
+end)
+
+Tab1Section:NewButton("Keyboard Script", "Execute Script", function()
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+
+end)
+
+Tab1Section:NewKeybind("ToggleUI", "Ui Close and Off", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
+Tab1Section:NewSlider("WalkSpeed", "Speed On Humanoid", 500, 0, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
